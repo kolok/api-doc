88e5c4d34358b17261f2b2bab5ad5c87308d7774
Nicolas Oudard <noudard@weborama.com>
2016-11-13 10:40:24 +0100

    update sql population

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories`  (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `label` varchar(255) NOT NULL DEFAULT '',
@@ -5,18 +6,19 @@ CREATE TABLE `categories`  (
       UNIQUE KEY `label` (`label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `pages`;
 CREATE TABLE `pages`  (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `category_id` int(11) NOT NULL,
       `parent_id` int(11) DEFAULT NULL,
       `label` varchar(255) NOT NULL DEFAULT '',
-      `content` varchar(255) NOT NULL DEFAULT '',
+      `content` text NOT NULL DEFAULT '',
       PRIMARY KEY (`id`),
       UNIQUE KEY `label` (`label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `categories` (id,label) VALUES (1, 'cat1'),(2, 'cat2'),(3, 'cat3');
+INSERT INTO `categories` (id,label) VALUES (1, 'cat1'),(2, 'cat2'),(3, 'cat3 - without pages');
 
-INSERT INTO `pages` (id,category_id,parent_id,label,content) VALUES (1, 1, null, "WCM API doc", "<h1>WCM API doc</h1><h2>Getting started</h2><p>How do you start ?<br>please follow this step...</p>"),(2, 1, 1, "WCM Structure", "<h1>WCM structure</h1><h2>Authentication Structure</h2><p>structure description...</p><h2>WCM product Structure</h2><p>structure description...</p>"),(3, 1, 1, "WCM API routes", "<h1>WCM routes</h1><h2>Account routes</h2><p>routes description</p>"),(4, 2, null, "WAM API doc", "<h1>WAM API doc</h1><h2>Getting started</h2><p>How do you start ?<br>please follow this step...</p>");
+INSERT INTO `pages` (id,category_id,parent_id,label,content) VALUES (1, 1, null, "Category 1 page 1", "<h1>First page (cat 1)</h1><h2>Getting started</h2><p>How do you start ?<br>please follow this step...</p>"),(2, 1, 1, "Category 1 Page 2", "<h1>Second page (cat 2)</h1><h2>Where am I ?</h2><p>This page is the second page of category 1</p><p>It is also the child of page 1 in the same category</p><h2>How it works ?</h2><p>The page belongs to a category, its title is displayed on the left when its category is displayed. And the pages of a category have a hierarchy using parent_id which point on an other page of the categoiry. This provide a tree (displayed on the left) !!! Each page handle an order which is used to be ordered on the tree on each level</p>"),(3, 1, 1, "Category 1 - Page 3", "<h1>More details</h1><h2>sub title of more details</h2><p>check the <a onClick='return false;'>cat 2</a></p>"),(4, 2, null, "Page 1 of category 2", "<h1>Page 1 (cat 2)</h1><h2>Getting started</h2><p>This is my cat 2<br>please follow this step...</p>");
 
 
